5a47df304682a7c65d43f389af898591ebea1622
Toyin Adegoke <gokety1@gmail.com>
2016-11-26 15:26:13 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created by hp on 10/22/2016.
+ */
+$(function(){
+    /*this is is what happens when the button is fired*/
+    $('#btn-click').click(function(){
+        /*the display-modal class is removed*/
+        $('#play-modal').removeClass('play-modal');
+        /*the display-modal-preview is added class is added*/
+        $('#play-modal').addClass('display-modal-preview animated fadeIn');
+
+    })
+})
+
+$(function(){
+    /*the span X in the body is clicked here*/
+    $('.modal-close').click(function(){
+        /*the display-modal-7 class is removed*/
+        $('#play-modal').removeClass('display-modal-preview');
+        /*the display-modal class is added*/
+        $('#play-modal').addClass('play-modal animated fadeOut');
+
+        /*paparazi*/
+        $('#play-modal').removeClass('animated fadeOut');
+    })
+
+})
+
+//login-form
+$(function(){
+    $('#login-click').click(function(){
+       
+        $('#play-modal').addClass('play-modal');
+        
+        $('#login-modal').removeClass('play-modal');
+        
+        
+        $('#login-modal').addClass('display-modal-preview animated fadeIn');
+    })
+})
+
+$(function(){
+    /*the span X in the body is clicked here*/
+    $('.modal-close').click(function(){
+        /*the display-modal-7 class is removed*/
+        $('#login-modal').removeClass('display-modal-preview');
+        /*the display-modal class is added*/
+        $('#login-modal').addClass('play-modal animated fadeOut');
+
+        /*paparazi*/
+        $('#login-modal').removeClass('animated fadeOut');
+    })
+
+})
